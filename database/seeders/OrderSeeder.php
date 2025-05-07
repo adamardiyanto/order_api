71726a825f76fa4efa90f7a938b754ede802daf4
adam <adamardiyantow@gmail.com>
2025-05-07 10:05:17 +0700

    Add models, factories, controllers, and seeders for Client and Order entities

---
+++ database/seeders/OrderSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Order;
+use App\Models\Client;
+
+class OrderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $clients = Client::all();
+
+        foreach ($clients as $client) {
+            Order::factory()->count(2)->create([
+                'client_id' => $client->id,
+            ]);
+        }
+    }
+}
